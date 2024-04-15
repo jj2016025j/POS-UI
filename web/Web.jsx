d241dc81e62dbf2a3f37f93179bdd80f38bcfe22
jj2016025j <103946700+jj2016025j@users.noreply.github.com>
2024-04-15 19:06:21 +0800

    轉移到第一頁

--- web/Web.jsx
+++ web/Web.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Route, Switch, useRouteMatch, Redirect } from 'react-router-dom';
-import Home from '../react-app/src/page/forWeb/Home';
-import News from '../react-app/src/page/forWeb/News';
+import Home from '../POS-UI/src/page/forWeb/Home';
+import News from '../POS-UI/src/page/forWeb/News';
 // import Menu_Base from '../page/forWeb/Menu_Base';
 // import Menu_Meat from '../page/forWeb/Menu_Meat';
 
-import About from '../react-app/src/page/forWeb/AboutUs';
-import Contact from '../react-app/src/page/forWeb/ContactUs';
+import About from '../POS-UI/src/page/forWeb/AboutUs';
+import Contact from '../POS-UI/src/page/forWeb/ContactUs';
 
-import Nav from '../react-app/src/components/forWeb/Nav';
-import Footer from '../react-app/src/components/forWeb/Footer';
+import Nav from '../POS-UI/src/components/forWeb/Nav';
+import Footer from '../POS-UI/src/components/forWeb/Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 // 其他導入...
