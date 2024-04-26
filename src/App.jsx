ae8989743c38e518adb1677fe34ab5f6bbe2a17b
jj2016025j <103946700+jj2016025j@users.noreply.github.com>
2024-04-26 16:38:41 +0800

    解決側邊欄在不同路徑時的顯示問題

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,11 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
-import TablesStatus from './page/TablesStatus';
-import Menu from './page/Menu';
-import ViewOrder from './page/ViewOrder';
-import ConfirmSubOrder from './page/ConfirmSubOrder';
-import PhoneOrder from './page/PhoneOrder';
-import PhoneConfirmorder from './page/PhoneConfirmorder';
-import PhoneCompleteOrder from './page/PhoneCompleteOrder';
-import Edit from './page/Edit';
-import Report from './page/Report';
-import Checkout from './page/Checkout';
-import OrderHistory from './page/OrderHistory';
-import RoleManagement from './page/RoleManagement';
-import Settings from './page/Settings';
+import { BrowserRouter } from 'react-router-dom';
 
-import Nav from './components/Nav';
-import Error from './page/Error';
+import MyRouter from './components/MyRouter';
 import Header from './components/Header';
+import Nav from './components/Nav';
 
 import { CartProvider } from './contexts/CartContext';
-import ShoppingCart from './components/ShoppingCart';
-import EditArea from './components/EditArea';
-
 import { EditProvider } from './contexts/EditContext';
 
 function App() {
@@ -32,31 +16,7 @@ function App() {
                     <div className="myBody">
                         <Header />
                         <Nav />
-                        <section>
-                            <Switch>
-                                <Redirect exact from={`/`} to={`/pos`} />
-                                <Route exact path={`/pos`} component={TablesStatus} />
-                                <Route path={`/order/:mainOrderId`} component={Menu} />
-                                <Route path={`/vieworder/:mainOrderId`} component={ViewOrder} />
-                                <Route path={`/confirmsuborder/:mainOrderId`} component={ConfirmSubOrder} />
-                                <Route path={`/phoneorder/:mainOrderId`} component={PhoneOrder} />
-                                <Route path={`/phoneconfirmorder/:mainOrderId`} component={PhoneConfirmorder} />
-                                <Route path={`/phoneCompleteOrder/:mainOrderId`} component={PhoneCompleteOrder} />
-                                <Route path={`/checkout/:mainOrderId`} component={Checkout} />
-                                <Route path={`/orderhistory`} component={OrderHistory} />
-                                <Route path={`/editMenuItem`} component={Edit} />
-                                <Route path={`/report`} component={Report} />
-                                <Route path="/role" component={RoleManagement} />
-                                <Route path="/setting" component={Settings} />
-                                <Route component={Error} />
-                            </Switch>
-                        </section>
-                        <aside>
-                            <Switch>
-                                <Route path={`/order/:mainOrderId`} component={ShoppingCart} />
-                                <Route path={`/editMenuItem`} component={EditArea} />
-                            </Switch>
-                        </aside >
+                        <MyRouter />
                     </div>
                 </BrowserRouter>
             </CartProvider>
